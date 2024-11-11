0bf66f4ad903526e4f4970816534612eac8288e4
lethanhtuan939 <thanhtuanle939@gmail.com>
2024-11-11 20:11:22 +0700

    update experience

--- src/app/components/home/experience/experience.component.ts
+++ src/app/components/home/experience/experience.component.ts
@@ -9,6 +9,19 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 export class ExperienceComponent implements OnInit {
   Experiences: any[] = [
     {
+      Tab: 'Work',
+      Orgenization: 'Techzen company limited',
+      Title: 'Fullstack Developer Intern',
+      Date: '19/08/2024 - 11/10/2024',
+      Description: [
+        'I am a fullstack developer intern at Techzen company limited. During my internship, I have been working on a project to build an online learning platform website for the company. I am responsible for the front-end (VueJs) and back-end (Spring boot) of the website. I have gained a lot of experience in web development and teamwork follow Agile Scrum methodology.',
+      ],
+      Link: [
+        'https://github.com/lethanhtuan939/Techlearn-BE',
+        'https://github.com/lethanhtuan939/Techlearn-FE'
+      ]
+    },
+    {
       Tab: 'Education',
       Orgenization: 'University of Technology and Education',
       Title: 'IT Student',
